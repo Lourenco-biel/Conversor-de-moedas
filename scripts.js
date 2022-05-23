9e33bed991c2c47196246db744c03570c6a1301d
Gabriel Lourenço <lourencogabriel77@gmail.com>
2022-05-23 14:39:38 -0300

    Pegando dados de uma api

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 const button = document.getElementById(`convert-button`)
 const select = document.getElementById(`currency-select`)
 
-const dolar = 5.2
-const euro = 5.9
-const bit = (1000 * 300)
 
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById(`input-real`).value
     const realvaluetext = document.getElementById(`real-value-text`)
     const currencyvaluetext = document.getElementById(`currency-value-text`)
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bit = data.BTCBRL.high
+
 
     realvaluetext.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
